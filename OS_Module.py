edfe0e280d62aaadedc3994a0e3d0b3a77289273
debojit bhadra <debojit.bhadra@gmail.com>
2018-08-25 13:11:54 +0530

    OS Module

--- OS_Module.py
+++ OS_Module.py
@@ -20,12 +20,16 @@ print(os.listdir())
 
 
 #MakeDirectory and Remove Diectory
-#os.mkdir('Python-Test-Directory')
-#os.rmdir('Python-Test-Directory')
-#os.makedirs('Python-Test-Directory-001/Python-Test-SubDirectory-001/Python-Test-2SubDirectory-001')
-#os.removedirs('Python-Test-Directory-001/Python-Test-SubDirectory-001/Python-Test-2SubDirectory-001')
+#Create Single Directory
+os.mkdir('Python-Test-Directory') 
+#Remove Single Directory
+os.rmdir('Python-Test-Directory') 
+#Make Recursive Directory
+os.makedirs('Python-Test-Directory-001/Python-Test-SubDirectory-001/Python-Test-2SubDirectory-001')
+#Delete Recursive Directory
+os.removedirs('Python-Test-Directory-001/Python-Test-SubDirectory-001/Python-Test-2SubDirectory-001')
 
-
+'''
 #Traveresing through Directory
 print("------------------------------------------")
 print("Walking through directory")
@@ -36,5 +40,12 @@ for dirpath,dirname,filenames in os.walk('c:/Users/Savit/Ruby'):
 	print('Directory Name: {}'.format(dirname))
 	print("File-Name: {}".format(filenames))
 	print("------------------------------------------")
+'''
 
+#Python OS.path 
+
+print(os.path.exists("No_suct_file.txt"))#Returns Bolean True or False  =>False
+print((os.path.basename("C:\\Users\\Savit\\python\\Os_Module.py"))) #Reten the File name OS_Modile.txt
+print(os.path.splitext("Os_Module.py")) #Reurn the file Name and the extention ('Os_Module', '.py')
+print(os.path.join("C:\\Users\\Savit\\python","Os_Module.py"))#joins the File Name with Path
 
